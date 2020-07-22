c65e5274c89c23a51b78b3b31abdea16cb9beb04
Brandon Unzueta <unzueta.brandon@gmail.com>
2020-07-21 20:27:43 -0700

    adding initial page files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
 
 function App() {
-  return <h1>Nice</h1>;
+  return (
+    <div>
+      <Home />
+      <Rooms />
+      <h1>Matrix code for one last time?</h1>
+    </div>
+  );
 }
 
 export default App;
