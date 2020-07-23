02578d84688e706a1830355f0fae0340d7cb04f2
Brandon Unzueta <unzueta.brandon@gmail.com>
2020-07-22 20:15:40 -0700

    creating Hero and Banner components

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,5 +1,16 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function Rooms() {
-  return <div>Rooms Page</div>;
+  return (
+    <Hero hero="roomsHero">
+      <Banner title="our rooms" subtitle="">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
