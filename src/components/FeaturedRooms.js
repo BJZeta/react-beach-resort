b17885e4c3ea14d94257ce7d0bdbf02573d6ac79
Brandon Unzueta <unzueta.brandon@gmail.com>
2020-07-26 20:40:17 -0700

    FeaturedRooms.js and Loading.js

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import { RoomContext } from "../contex";
+import Loading from "./Loading";
+import Room from "./Room";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
 
   render() {
-    const value = this.context;
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
 
-    return <div>Featured Rooms</div>;
+    return (
+      <div>
+        Featured Rooms
+        <Loading />
+      </div>
+    );
   }
 }
